26b715869b6a4411f13fabe79b3626d7c830f801
Anton Smirnov <tosha.smirnov.2002@gmail.com>
2023-02-25 17:47:03 +0300

    project configuration

--- apps/web-client/pages/_app.tsx
+++ apps/web-client/pages/_app.tsx
@@ -1,18 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to web-client!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Welcome to web-client!</title>
+            </Head>
+            <main className="app">
+                <ChakraProvider>
+                    <Component {...pageProps} />
+                </ChakraProvider>
+            </main>
+        </>
+    );
 }
 
 export default CustomApp;
